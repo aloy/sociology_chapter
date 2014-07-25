63f18afd57b4e80fc4f371c84eacea30e1b59e54
Heike Hofmann <hofmann@iastate.edu>
2014-07-25 16:16:37 -0500

    update to new gridSVG

--- R/add_interaction.R
+++ R/add_interaction.R
@@ -1,4 +1,5 @@
-make_interactive <- function(filename, script, toggle="toggle") {
+make_interactive <- function(filename, script, toggle="toggle", background="#e5e5e5") {
+# use #ffffff for white background
   require(gridSVG)
   grobs <- grid.ls()
   
@@ -7,7 +8,7 @@ make_interactive <- function(filename, script, toggle="toggle") {
     grid.garnish(grobs$name[i],
                  onmouseover=paste("frame('",grobs$name[i+2], ".1')", sep=""),
                  onmouseout=paste("deframe('",grobs$name[i+2], ".1')", sep=""), 
-                 onmousedown=paste(sprintf("%shigh(evt, '", toggle),grobs$name[i+2], ".1')", sep=""))
+                 onmousedown=paste(sprintf("%shigh(evt, '", toggle, background),grobs$name[i+2], ".1')", sep=""))
   }
 
   # use script on server to get locally executable javascript code
